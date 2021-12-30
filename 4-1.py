32c77c83ce9294a050048d0fd2e15f4693c4967d
Minsoo Kim <pos02043@gmail.com>
2021-12-31 01:13:20 +0900

    Add files via upload

---
+++ 4-1.py
@@ -0,0 +1,28 @@
+
+l = int(input())
+a = list(input().split())
+
+x=1
+y=1
+
+for i in a:
+  if i=="R":
+    if x+1>=1 and x+1<=l:
+      x+=1
+
+  if i=="L":
+    if x-1>=1 and x-1<=l:
+      x-=1
+
+  if i=="U":
+    if y-1>=1 and y-1<=l:
+      y-=1
+
+  if i=="D":
+    if y+1>=1 and y+1<=l:
+      y+=1
+
+print(y,x) #O(len(a))
+
+
+
