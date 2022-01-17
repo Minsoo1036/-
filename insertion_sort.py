dfa6c9ea43eec83da2af1cf832ec6bfead9a8269
Minsoo Kim <pos02043@gmail.com>
2022-01-17 18:14:07 +0900

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+#insertion sort (삽입 정렬)
+
+array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(1,len(array)):
+  for j in range(i,0,-1): #등차가 음수일 시, end+1까지.
+    if array[j] < array[j-1]:
+      array[j],array[j-1] = array[j-1],array[j]
+    else:
+      break
+
+print(array)
