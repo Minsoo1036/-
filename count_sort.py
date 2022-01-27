90196bfb12a16b1966ff1e2b9b55901dace51b1d
Minsoo Kim <pos02043@gmail.com>
2022-01-27 16:59:34 +0900

    Add files via upload

---
+++ count_sort.py
@@ -0,0 +1,13 @@
+#Count sort, O(N+K) time , N:array size, K:maxima
+
+array = [7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
+
+count = [0]*(max(array)+1)
+
+for i in range(len(array)):
+  count[array[i]] += 1
+
+for i in range(len(count)):
+  for j in range(count[i]):
+    print(i, end=' ')
+
